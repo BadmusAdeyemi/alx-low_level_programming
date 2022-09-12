8410f3591b67e983e01436c758f3e29b3997321c
BadmusAdeyemi <bysodiq@gmail.com>
2022-09-11 23:04:51 -0700

    Debug

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = 0; ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
-			if (!(tens == '9' && ones == 9))
+			if (!(tens == '9' && ones == '9'))
 			{
 				putchar(',');
 				putchar(' ');
