49ba8300c24cfd864a22db454fe8a050d2636cc3
BadmusAdeyemi <bysodiq@gmail.com>
2022-09-13 14:01:33 -0700

    6th commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,16 +10,4 @@
 int add(int a, int b)
 {
 	return (a + b);
-}#include "holberton.h"
-
-/**
- * add - add two integers
- * @a: two integer arguments
- * @b: two integer arguments
- * Return: sum
- */
-
-int add(int a, int b)
-{
-	return (a + b);
 }
