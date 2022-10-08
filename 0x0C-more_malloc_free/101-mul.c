cfdd05b1a3dc7ad04c53e63dba2c1ffd2356fe79
BadmusAdeyemi <bysodiq@gmail.com>
2022-10-07 23:41:58 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - print product of argument numbers
  * @argc: argument counter
  * @argv: numbers to multiply
- * Return: 0 on success, 98 if two arguments not given
+ * Return: 0 on success, 1 if two arguments not given
  */
 
 int main(int argc, char *argv[])
@@ -17,8 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (98);
 	}
-	if  ((atoi(argv[1]) >= '0' && atoi(argv[1]) <= '9') || (atoi(argv[2]) >= '0' && atoi(argv[2]) <= '9'))
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
